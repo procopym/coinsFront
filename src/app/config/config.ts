16e80dfdeaeb880db9de5d65e11a7820354834ea
Mihai Procopi <procopy.mihai@gmail.com>
2019-10-02 20:20:49 +0300

    Add config to category api

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -2,5 +2,6 @@ const hostName = 'http://localhost:3000';
 export let CONFIG = {
   getTransactionData: `${hostName}/getTransactionData`,
   getUserCounts:`${hostName}/getUserData`,
-  removeTransaction:`${hostName}/removeTransaction`
+  removeTransaction:`${hostName}/removeTransaction`,
+  removeCategory:`${hostName}/removeCategory`
 };
