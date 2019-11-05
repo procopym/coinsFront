e86e6f4eb814810c58712a016dae6c1b1cb34ac6
Mihai Procopi <procopy.mihai@gmail.com>
2019-11-05 20:09:46 +0200

    add statistics charts components

--- src/app/pages/statistics-bar/statistics-bar.component.ts
+++ src/app/pages/statistics-bar/statistics-bar.component.ts
@@ -1,7 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import {Component, OnInit} from '@angular/core';
+import {ChartOptions, ChartType, ChartDataSets} from 'chart.js';
 import * as pluginDataLabels from 'chartjs-plugin-datalabels';
-import { Label } from 'ng2-charts';
+import {Label} from 'ng2-charts';
+import {ApiService} from "../../services/api.service";
+import {userId} from "../../config/config";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+interface recByYear {
+  r_period: string,
+  r_data_type: string,
+  r_amount: number
+}
 
 @Component({
   selector: 'app-bar-chart',
@@ -9,10 +18,19 @@ import { Label } from 'ng2-charts';
   styleUrls: ['./statistics-bar.component.css'],
 })
 export class StatisticsBarComponent implements OnInit {
+  maxDate = new Date();
+  statsRec: recByYear[];
+  dataIncome: number[];
+  dataOutcome: number[];
+  request: FormGroup;
+
+
+  // subscriber: any;
   public barChartOptions: ChartOptions = {
     responsive: true,
+
     // We use these empty structures as placeholders for dynamic theming.
-    scales: { xAxes: [{}], yAxes: [{}] },
+    scales: {xAxes: [{}], yAxes: [{}]},
     plugins: {
       datalabels: {
         anchor: 'end',
@@ -20,39 +38,94 @@ export class StatisticsBarComponent implements OnInit {
       }
     }
   };
-  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartLabels: Label[] = [];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartPlugins = [pluginDataLabels];
 
-  public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
-  ];
+  public barChartData: ChartDataSets[] = [{}];
 
-  constructor() { }
+  constructor(private api: ApiService) {
+  }
 
   ngOnInit() {
+    this.iniForm();
+    this.initStats();
   }
+
+  get dateControl() {
+    return this.request.get('date');
+  }
+
+  private iniForm(): void {
+    const date = this.maxDate;
+    // console.log(date);
+    this.request = new FormGroup({
+      date: new FormControl(date, [Validators.required])
+    });
+  }
+
+  public getDate(date) {
+    let dateObj = new Date(date);
+    return `${dateObj.getFullYear()}-${dateObj.getMonth() + 1}-${dateObj.getDate()}`;
+  }
+
   // events
-  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+  public chartClicked({event, active}: { event: MouseEvent, active: {}[] }): void {
     console.log(event, active);
   }
 
-  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+  public chartHovered({event, active}: { event: MouseEvent, active: {}[] }): void {
     console.log(event, active);
   }
 
-  public randomize(): void {
-    // Only Change 3 values
-    const data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      (Math.random() * 100),
-      56,
-      (Math.random() * 100),
-      40];
-    this.barChartData[0].data = data;
+  private initCanvas(): ChartDataSets[] {
+    this.dataIncome = [];
+    this.dataOutcome = [];
+    this.barChartLabels = [];
+    this.statsRec.forEach(el => {
+      this.barChartLabels.push(el.r_period);
+    });
+    this.statsRec.forEach(el => {
+      if (el.r_data_type === "income") {
+        this.dataIncome.push(el.r_amount);
+      }
+      if (el.r_data_type === "outcome") {
+        this.dataOutcome.push(el.r_amount);
+      }
+
+      if (this.dataIncome.length < this.dataOutcome.length) {
+        this.dataIncome.push(null);
+      } else if (this.dataIncome.length > this.dataOutcome.length) {
+        this.dataOutcome.push(null);
+      }
+    });
+    return [
+      {data: this.dataIncome, label: 'Income'},
+      {data: this.dataOutcome, label: 'Outcome'}
+    ]
+  }
+
+  private initStats(): void {
+    this.api.getStatsByYear({
+      userId: userId,
+      pivotDate: this.getDate(this.request.value.date || this.maxDate)
+    }).subscribe((response: { success, data }) => {
+      if (response.success) {
+        this.statsRec = response.data;
+        this.barChartData = this.initCanvas();
+      }
+    });
+  }
+
+  onSubmit() {
+    if (this.request.valid) {
+      this.initStats();
+    }
+  }
+
+  onReset() {
+    this.iniForm();
+    this.initStats();
   }
 }
--- src/app/pages/statistics-pie/statistics-pie.component.ts
+++ src/app/pages/statistics-pie/statistics-pie.component.ts
@@ -2,6 +2,16 @@ import {Component, OnInit} from '@angular/core';
 import {ChartType, ChartOptions} from 'chart.js';
 import {Label} from 'ng2-charts';
 import * as pluginDataLabels from 'chartjs-plugin-datalabels';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ApiService} from "../../services/api.service";
+import {userId} from "../../config/config";
+
+interface StatsRec {
+  r_category_name: string,
+  r_tot_amount: number,
+  r_from_date: string,
+  r_to_date: string
+}
 
 @Component({
   selector: 'app-statistics-pipe',
@@ -9,11 +19,16 @@ import * as pluginDataLabels from 'chartjs-plugin-datalabels';
   styleUrls: ['./statistics-pie.component.css']
 })
 export class StatisticsPieComponent implements OnInit {
+  maxDate = new Date();
+  statsRec: StatsRec[];
+  request: FormGroup;
+  period: string;
+
 // Pie
   public pieChartOptions: ChartOptions = {
     responsive: true,
     legend: {
-      position: 'top',
+      position: 'left',
     },
     plugins: {
       dataLabels: {
@@ -24,55 +39,96 @@ export class StatisticsPieComponent implements OnInit {
       },
     }
   };
-  public pieChartLabels: Label[] = [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'];
-  public pieChartData: number[] = [300, 500, 100];
+  public pieChartLabels: Label[] = [];
+  public pieChartData: number[] = [];
   public pieChartType: ChartType = 'pie';
   public pieChartLegend = true;
   public pieChartPlugins = [pluginDataLabels];
-  public pieChartColors = [
-    {
-      backgroundColor: ['rgba(255,0,0,0.3)', 'rgba(0,255,0,0.3)', 'rgba(0,0,255,0.3)'],
-    },
-  ];
+  public pieChartColors = [{backgroundColor: [],},];
 
-  constructor() {
+  constructor(private api: ApiService) {
   }
 
   ngOnInit() {
+    this.iniForm();
+    this.initStats()
+  }
+
+  get dateControl() {
+    return this.request.get('date');
+  }
+
+  get radioBtnControl() {
+    return this.request.get('radioBtn');
+  }
+
+  private iniForm() {
+    const date = this.maxDate;
+    const radioBtn = "0";
+    this.request = new FormGroup({
+      date: new FormControl(date, [Validators.required]),
+      radioBtn: new FormControl(radioBtn, [Validators.required])
+    });
+  }
+
+  private initStats(): void {
+    this.api.getStatsPerMonth({
+      userId: userId,
+      transactionType: (Number(this.request.value.radioBtn) || 0),
+      pivotDate: this.getDate(this.request.value.date || this.maxDate)
+    }).subscribe((response: { success, data }) => {
+      if (response.success) {
+        this.statsRec = response.data;
+        this.initCanvas();
+      }
+    });
+  }
+
+  private initCanvas(): void {
+    this.pieChartLabels = [];
+    this.pieChartData = [];
+    this.pieChartColors[0].backgroundColor = [];
+    this.period = this.statsRec.length > 0 ? this.statsRec[0].r_from_date + '  -  ' + this.statsRec[0].r_to_date : null;
+    this.statsRec.forEach(el => {
+      // console.log(el);
+      this.pieChartLabels.push(el.r_category_name);
+      this.pieChartData.push(el.r_tot_amount);
+      this.pieChartColors[0].backgroundColor.push(`rgb(${this.getRandomInt(256, 120)},${this.getRandomInt(256, 120)},${this.getRandomInt(256, 120)}, 0.7)`);
+    });
+  }
+
+  public getDate(date) {
+    let dateObj = new Date(date);
+    return `${dateObj.getFullYear()}-${dateObj.getMonth() + 1}-${dateObj.getDate()}`;
+  }
+
+  onReset() {
+    this.iniForm();
+    this.initStats();
+  }
+
+  onSubmit() {
+    if (this.request.valid) {
+      this.initStats();
+    }
+  }
+
+  private getRandomInt(max, min = 0) {
+    return Math.floor(Math.random() * Math.floor(max - min) + Math.floor(min));
   }
 
   // events
-  public chartClicked({event, active}: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
+  public chartClicked(event: MouseEvent): void {
+    this.pieChartLegend = !this.pieChartLegend;
+    console.log("Hello from @mihaiprocopi");
   }
 
   public chartHovered({event, active}: { event: MouseEvent, active: {}[] }): void {
     console.log(event, active);
   }
 
-  changeLabels() {
-    const words = ['hen', 'variable', 'embryo', 'instal', 'pleasant', 'physical', 'bomber', 'army', 'add', 'film',
-      'conductor', 'comfortable', 'flourish', 'establish', 'circumstance', 'chimney', 'crack', 'hall', 'energy',
-      'treat', 'window', 'shareholder', 'division', 'disk', 'temptation', 'chord', 'left', 'hospital', 'beef',
-      'patrol', 'satisfied', 'academy', 'acceptance', 'ivory', 'aquarium', 'building', 'store', 'replace', 'language',
-      'redeem', 'honest', 'intention', 'silk', 'opera', 'sleep', 'innocent', 'ignore', 'suite', 'applaud', 'funny'];
-    const randomWord = () => words[Math.trunc(Math.random() * words.length)];
-    this.pieChartLabels = Array.apply(null, {length: 3}).map(_ => randomWord());
-  }
-
-  addSlice() {
-    this.pieChartLabels.push(['Line 1', 'Line 2', 'Line 3']);
-    this.pieChartData.push(400);
-    this.pieChartColors[0].backgroundColor.push('rgba(196,79,244,0.3)');
-  }
-
-  removeSlice() {
-    this.pieChartLabels.pop();
-    this.pieChartData.pop();
-    this.pieChartColors[0].backgroundColor.pop();
-  }
-
   changeLegendPosition() {
-    this.pieChartOptions.legend.position = this.pieChartOptions.legend.position === 'left' ? 'top' : 'left';
+    this.pieChartOptions.legend.position = this.pieChartOptions.legend.position === 'left' ? 'right' : 'left';
   }
+
 }
